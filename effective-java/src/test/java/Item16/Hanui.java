5256614922989b19e63c26c4dc11edc57af021ea
kimhanui <30483337+kimhanui@users.noreply.github.com>
2021-09-23 17:37:29 +0900

    Item16 정리글 추가

---
+++ effective-java/src/test/java/Item16/Hanui.java
@@ -0,0 +1,13 @@
+package Item16;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용하라")
+public class Hanui {
+
+    @Test
+    public void test(){
+
+    }
+}
